52fb277bf4ed64f0f84b7f89e0da29fcac748a1f
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-04-21 19:13:54 +0300

    .
    .

---
+++ View/MapsPin.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace DonemProje.View
+{
+    public class MapsPin : ContentPage
+    {
+        public MapsPin()
+        {
+            Title = "Pins demo";
+            Position position = new Position(38.5002, 27.7084);
+            MapSpan mapSpan = new MapSpan(position, 0.01, 0.01);
+            Map map = new Map(mapSpan);
+
+            Pin pin = new Pin
+            {
+                Label = "Manisa",
+                Address = "Hasan Ferdi Turgutlu Teknoloji Fakültesi",
+                Type = PinType.Place,
+                Position = position
+            };
+            map.Pins.Add(pin);
+            Content = new StackLayout
+            {
+                Margin = new Thickness(10),
+                Children =
+                {
+                    map,
+                }
+            };
+        }
+    }
+}
