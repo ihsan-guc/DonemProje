030939d7f45279957cb875044b553985d016ab54
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-05-02 01:56:25 +0300

    gRPC
    gRPC Ekleme.

---
+++ HelloworldXamarin/HelloworldGrpc.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using grpc = global::Grpc.Core;
+
+namespace DonemProje.HelloworldXamarin
+{
+    public class HelloworldGrpc
+    {
+        public static partial class Greeter
+        {
+            static readonly string __ServiceName = "helloworld.Greeter";
+
+            static readonly grpc::Marshaller<global::DonemProje.HelloworldXamarin.Helloworld.HelloRequest> __Marshaller_HelloRequest = grpc::Marshallers.Create((arg) => global::Google.Protobuf.MessageExtensions.ToByteArray(arg), global::DonemProje.HelloworldXamarin.Helloworld.HelloRequest.Parser.ParseFrom);
+            static readonly grpc::Marshaller<global::DonemProje.HelloworldXamarin.Helloworld.HelloReply> __Marshaller_HelloReply = grpc::Marshallers.Create((arg) => global::Google.Protobuf.MessageExtensions.ToByteArray(arg), global::DonemProje.HelloworldXamarin.Helloworld.HelloReply.Parser.ParseFrom);
+
+            static readonly grpc::Method<global::DonemProje.HelloworldXamarin.Helloworld.HelloRequest, global::DonemProje.HelloworldXamarin.Helloworld.HelloReply> __Method_SayHello = new grpc::Method<global::DonemProje.HelloworldXamarin.Helloworld.HelloRequest, global::DonemProje.HelloworldXamarin.Helloworld.HelloReply>(
+                grpc::MethodType.Unary,
+                __ServiceName,
+                "SayHello",
+                __Marshaller_HelloRequest,
+                __Marshaller_HelloReply);
+            public static global::Google.Protobuf.Reflection.ServiceDescriptor Descriptor
+            {
+                get { return global::DonemProje.HelloworldXamarin.Helloworld.HelloworldReflection.Descriptor.Services[0]; }
+            }
+            public abstract partial class GreeterBase
+            {
+                public virtual global::System.Threading.Tasks.Task<global::DonemProje.HelloworldXamarin.Helloworld.HelloReply> SayHello(global::DonemProje.HelloworldXamarin.Helloworld.HelloRequest request, grpc::ServerCallContext context)
+                {
+                    throw new grpc::RpcException(new grpc::Status(grpc::StatusCode.Unimplemented, ""));
+                }
+
+            }
+            public partial class GreeterClient : grpc::ClientBase<GreeterClient>
+            {
+                public GreeterClient(grpc::Channel channel) : base(channel)
+                {
+                }
+                public GreeterClient(grpc::CallInvoker callInvoker) : base(callInvoker)
+                {
+                }
+                protected GreeterClient() : base()
+                {
+                }
+                protected GreeterClient(ClientBaseConfiguration configuration) : base(configuration)
+                {
+                }
+                public virtual global::DonemProje.HelloworldXamarin.Helloworld.HelloReply SayHello(global::DonemProje.HelloworldXamarin.Helloworld.HelloRequest request, grpc::Metadata headers = null, DateTime? deadline = null, CancellationToken cancellationToken = default(CancellationToken))
+                {
+                    return SayHello(request, new grpc::CallOptions(headers, deadline, cancellationToken));
+                }
+                public virtual global::DonemProje.HelloworldXamarin.Helloworld.HelloReply SayHello(global::DonemProje.HelloworldXamarin.Helloworld.HelloRequest request, grpc::CallOptions options)
+                {
+                    return CallInvoker.BlockingUnaryCall(__Method_SayHello, null, options, request);
+                }
+                public virtual grpc::AsyncUnaryCall<global::DonemProje.HelloworldXamarin.Helloworld.HelloReply> SayHelloAsync(global::DonemProje.HelloworldXamarin.Helloworld.HelloRequest request, grpc::Metadata headers = null, DateTime? deadline = null, CancellationToken cancellationToken = default(CancellationToken))
+                {
+                    return SayHelloAsync(request, new grpc::CallOptions(headers, deadline, cancellationToken));
+                }
+                public virtual grpc::AsyncUnaryCall<global::DonemProje.HelloworldXamarin.Helloworld.HelloReply> SayHelloAsync(global::DonemProje.HelloworldXamarin.Helloworld.HelloRequest request, grpc::CallOptions options)
+                {
+                    return CallInvoker.AsyncUnaryCall(__Method_SayHello, null, options, request);
+                }
+                protected override GreeterClient NewInstance(ClientBaseConfiguration configuration)
+                {
+                    return new GreeterClient(configuration);
+                }
+            }
+            public static grpc::ServerServiceDefinition BindService(GreeterBase serviceImpl)
+            {
+                return grpc::ServerServiceDefinition.CreateBuilder()
+                    .AddMethod(__Method_SayHello, serviceImpl.SayHello).Build();
+            }
+        }
+    }
+}
---
+++ View/gRPCPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using static DonemProje.HelloworldXamarin.Helloworld;
+
+namespace DonemProje.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class gRPCPage : ContentPage
+    {
+        const int Port = 50051;
+        int count = 1;
+        public gRPCPage()
+        {
+            InitializeComponent();
+            btn.Clicked += delegate { SayHello(btn); };
+        }
+        private void SayHello(Button button)
+        {
+            Server server = new Server
+            {
+                Services = { DonemProje.HelloworldXamarin.HelloworldGrpc.Greeter.BindService(new GreeterImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            // use loopback on host machine: https://developer.android.com/studio/run/emulator-networking
+            //10.0.2.2:50051
+            Channel channel = new Channel("localhost:50051", ChannelCredentials.Insecure);
+
+            var client = new DonemProje.HelloworldXamarin.HelloworldGrpc.Greeter.GreeterClient(channel);
+            string user = "Xamarin " + count;
+
+            var reply = client.SayHello(new DonemProje.HelloworldXamarin.Helloworld.HelloRequest { Name = user });
+
+            button.Text = "Greeting: " + reply.Message;
+
+            channel.ShutdownAsync().Wait();
+            server.ShutdownAsync().Wait();
+
+            count++;
+        }
+        class GreeterImpl : DonemProje.HelloworldXamarin.HelloworldGrpc.Greeter.GreeterBase
+        {
+            // Server side handler of the SayHello RPC
+            public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+            {
+                return Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
+            }
+        }
+    }
+}
